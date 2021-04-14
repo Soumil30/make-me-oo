ac3e51d5c76657ca1b93d4cad48a65837838872f
Soumil De <soumilde30@gmail.com>
2021-04-14 22:15:41 +0530

    Refactor. Calculation of square of difference of coordinates extracted to a method.

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -18,9 +18,14 @@ public class Point {
     }
 
     public double distanceFrom(Point anotherPoint) {
-        double xDistance = this.getX() - anotherPoint.getX();
-        double yDistance = this.getY() - anotherPoint.getY();
-        return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+        double squareOfDifferenceOfXCoordinates = squareOfDifference(this.getX(), anotherPoint.getX());
+        double squareOfDifferenceOfYCoordinates = squareOfDifference(this.getY(), anotherPoint.getY());
+        return Math.sqrt(squareOfDifferenceOfXCoordinates + squareOfDifferenceOfYCoordinates);
+    }
+
+    private double squareOfDifference(double coordinate1, double coordinate2) {
+        double distance = coordinate1 - coordinate2;
+        return Math.pow(distance, 2);
     }
 
     public double angleWith(Point anotherPoint) {
